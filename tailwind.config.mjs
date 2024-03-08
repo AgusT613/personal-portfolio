0e49dec13e30dd04e67ceeca4bdcc05ad47ea83e
AgusT613 <torresagustin613@gmail.com>
2024-03-08 15:39:44 -0300

    feature(copy-email-btn): change functionality and add animation-styles

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,17 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			keyframes: {
+				"message-disappear": {
+					'0%': { right: "0" },
+					'100%': { right: "-100%" }
+				}
+			},
+			animation: {
+				"message-disappear": "message-disappear 1.5s ease"
+			}
+		},
 	},
 	plugins: [],
 }
