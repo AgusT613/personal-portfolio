0ebb6d11b1f8e2150e52e6a02ceffa6ece94794c
AgusT613 <torresagustin613@gmail.com>
2024-04-03 16:04:28 -0300

    feat(i18n): add toggle lang to html-meta-data and header-section

---
+++ src/i18n/ui.ts
@@ -0,0 +1,75 @@
+export const languages = {
+    es: "es",
+    en: 'en',
+};
+  
+export const defaultLang = 'es';
+
+export const ui = {
+    es: {
+        // Head
+        "head.title": "Torres Agustin - Portafolio",
+        // Body
+            // Message Container
+        "message.container": "Email Copiado",
+            // Header
+        "header.top": "Inicio",
+        "header.experience": "Experiencia",
+        "header.projects": "Proyectos",
+        "header.about.me": "Sobre Mí",
+        "header.contact": "Contacto",
+            // Nav Items
+        "header.nav.items.top.id": "#top",
+        "header.nav.items.top.label": "top",
+        "header.nav.items.top.title": "Inicio",
+        //---
+        "header.nav.items.experience.id": "#experience",
+        "header.nav.items.experience.label": "experience",
+        "header.nav.items.experience.title": "Experiencia",
+        //---
+        "header.nav.items.projects.id": "#projects",
+        "header.nav.items.projects.label": "projects",
+        "header.nav.items.projects.title": "Proyectos",
+        //---
+        "header.nav.items.about.me.id": "#about-me",
+        "header.nav.items.about.me.label": "about-me",
+        "header.nav.items.about.me.title": "Sobre Mí",
+        //---
+        "header.nav.items.contact.id": "#contact",
+        "header.nav.items.contact.label": "contact",
+        "header.nav.items.contact.title": "Contacto",
+    },
+    en: {
+        // Head
+        "head.title": "Torres Agustin - Portfolio",
+        // Body
+            // Message Container
+        "message.container": "Email Copied",
+            // Header
+        "header.top": "Home",
+        "header.experience": "Experience",
+        "header.projects": "Projects",
+        "header.about.me": "About Me",
+        "header.contact": "Contact",
+            // Nav Items
+        "header.nav.items.top.id": "#top",
+        "header.nav.items.top.label": "top",
+        "header.nav.items.top.title": "Home",
+        //---
+        "header.nav.items.experience.id": "#experience",
+        "header.nav.items.experience.label": "experience",
+        "header.nav.items.experience.title": "Experience",
+        //---
+        "header.nav.items.projects.id": "#projects",
+        "header.nav.items.projects.label": "projects",
+        "header.nav.items.projects.title": "Projects",
+        //---
+        "header.nav.items.about.me.id": "#about-me",
+        "header.nav.items.about.me.label": "about-me",
+        "header.nav.items.about.me.title": "About Me",
+        //---
+        "header.nav.items.contact.id": "#contact",
+        "header.nav.items.contact.label": "contact",
+        "header.nav.items.contact.title": "Contact",
+    }
+} as const;
