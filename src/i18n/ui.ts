6fb5e873cba55b87068909e9852d84adf1aedf05
AgusT613 <torresagustin613@gmail.com>
2024-04-05 15:09:23 -0300

    feat(i18n): modify lang text of experience-section

--- src/i18n/ui.ts
+++ src/i18n/ui.ts
@@ -48,7 +48,25 @@ export const ui = {
         "hero.years.of.experience": "+1 año de experiencia.",
         "hero.description": "Estudiando desarrollo de software y colaborando en proyectos de terceros. Cuento con amplios conocimientos para la construcción de tu próximo proyecto web o aplicación.",
         "hero.download.cv": "Descargar CV",
-        "hero.download.cv.file.name": "torres-agustin-cv"
+        "hero.download.cv.file.name": "torres-agustin-cv",
+            // Experience Section
+        "experience.section.title": "Experiencia",
+            // Red Mascotera
+        "experience.red.mascotera.title": "Red Mascotera - Proyecto Solidario",
+        "experience.red.mascotera.date": "noviembre 2023 - actualmente",
+        "experience.red.mascotera.description": "A cargo de la construcción de la página web junto a un equipo de desarrolladores frontend. Reuniones con equipos de otras áreas técnicas para evaluar el estado, la mejora, y el avance del proyecto. Uso de herramientas de gestion de proyectos para la transparencia del trabajo con otras áreas de desarrollo.",
+            // ITBA
+        "experience.itba.title": "Escuela de Innovación ITBA - Programa de Capacitación Fullstack Developer",
+        "experience.itba.date": "agosto 2023 - noviembre 2023",
+        "experience.itba.description": "Diseño, maquetación, y desarrollo de un homebanking ficticio junto a un equipo de 3 compañeros. Proceso completo de construcción de la app desde la creación de la vista web hasta la implementación de una REST-API para la creación, modificación, y actualización de datos. A cargo del diseño, desarrollo de los endpoints de la API, y la conexión frontend-backend.",
+            // Don Bosco Labs
+        "experience.don.bosco.labs.title": "Instituto Web - Experiencia IT en Desarrollo de Software",
+        "experience.don.bosco.labs.date": "agosto 2023 - octubre 2023",
+        "experience.don.bosco.labs.description": "Maquetación y desarrollo de una plataforma para un sistema de laboratorio de análisis clínicos. A cargo de la construcción de la página de inicio o home-page. Participé en reuniones semanales para la evaluación de nuevas caracteristicas y el aprendizaje de otras áreas de desarrollo.",
+            // Femyp SRL
+        "experience.femyp.title": "Femyp SRL - Pasantia en Empresa de Ascensores",
+        "experience.femyp.date": "octubre 2022 - noviembre 2022",
+        "experience.femyp.description": "A cargo del diseño de piezas de ascensores, y de muebleria para la propuesta de la nueva oficina técnica de la empresa.",
     },
     en: {
         // Head
@@ -92,6 +110,24 @@ export const ui = {
         "hero.years.of.experience": "+1 year of experience.",
         "hero.description": "Studying software development and collaborating on third-party projects. I have extensive knowledge for building your next web project or application.",
         "hero.download.cv": "Resume",
-        "hero.download.cv.file.name": "torres-agustin-resume"
+        "hero.download.cv.file.name": "torres-agustin-resume",
+            // Experience Section
+        "experience.section.title": "Experience",
+            // Red Mascotera
+        "experience.red.mascotera.title": "Red Mascotera - Charitable Project",
+        "experience.red.mascotera.date": "november 2023 - present",
+        "experience.red.mascotera.description": "Leading the construction of the webpage alongside a team of frontend developers. Conducting meetings with teams from other technical areas to assess project status, identify areas for improvement, and monitor progress. Utilizing project management tools to facilitate transparent collaboration across development teams.",
+            // ITBA
+        "experience.itba.title": "Escuela de Innovación ITBA - Training Programme in FullStack Developer",
+        "experience.itba.date": "august 2023 - november 2023",
+        "experience.itba.description": "Designing, laying out, and developing a fictitious home banking platform with a team of three colleagues. Overseeing the entire app construction process, from creating the web interface to implementing a REST API for data creation, modification, and updates. Responsible for designing, developing API endpoints, and establishing frontend-backend connectivity.",
+            // Don Bosco Labs
+        "experience.don.bosco.labs.title": "Instituto Web - IT Experience in Software Development",
+        "experience.don.bosco.labs.date": "august 2023 - october 2023",
+        "experience.don.bosco.labs.description": "Laying out and developing a platform for a clinical analysis laboratory system. Responsible for constructing the homepage. Participated in weekly meetings to evaluate new features and gain insight into other areas of development.",
+            // Femyp SRL
+        "experience.femyp.title": "Femyp SRL - Internship in Elevator Company",
+        "experience.femyp.date": "october 2022 - november 2022",
+        "experience.femyp.description": "In charge of designing elevator components and furniture for the proposal of the company's new technical office.",
     }
 } as const;
