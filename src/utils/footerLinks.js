475a35bb404a6c200882456fc1e2bc3b42e3182b
Agus Torres <torresagustin613@gmail.com>
2025-07-28 16:20:06 -0300

    refactor: delete instagram link

--- src/utils/footerLinks.js
+++ src/utils/footerLinks.js
@@ -1,22 +1,18 @@
 import ICONS from "./icons";
 
 const FOOTER_LINKS = [
-    {
-        href: "mailto:torresagustin613@gmail.com",
-        icon: ICONS.Gmail
-    },
-    {
-        href: "https://www.linkedin.com/in/torres-facundo-agustin",
-        icon: ICONS.Linkedin
-    },
-    {
-        href: "https://www.instagram.com/agustorres613/",
-        icon: ICONS.Instagram
-    },
-    {
-        href: "https://github.com/AgusT613",
-        icon: ICONS.GitHub
-    }
-]
+  {
+    href: "mailto:torresagustin613@gmail.com",
+    icon: ICONS.Gmail,
+  },
+  {
+    href: "https://www.linkedin.com/in/torres-facundo-agustin",
+    icon: ICONS.Linkedin,
+  },
+  {
+    href: "https://github.com/AgusT613",
+    icon: ICONS.GitHub,
+  },
+];
 
-export default FOOTER_LINKS
+export default FOOTER_LINKS;
