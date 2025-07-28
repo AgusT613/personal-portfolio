ea4cb0699dbd676f44d5b504285b4b74d0db9fdc
Agus Torres <torresagustin613@gmail.com>
2025-07-28 17:08:13 -0300

    chore: separate language in different files

---
+++ src/i18n/lang/english.ts
@@ -0,0 +1,131 @@
+const en = {
+  // Head
+  "head.title": "Torres Agustin - Portfolio",
+  "head.description": "My personal web developer portfolio",
+  // Body
+  // Message Container
+  "message.container": "Email Copied",
+  // Header
+  "header.top": "Home",
+  "header.experience": "Experience",
+  "header.projects": "Projects",
+  "header.about.me": "About Me",
+  "header.contact": "Contact",
+  // Nav Items
+  "header.nav.items.top.id": "#top",
+  "header.nav.items.top.label": "top",
+  "header.nav.items.top.title": "Home",
+  //---
+  "header.nav.items.experience.id": "#experience",
+  "header.nav.items.experience.label": "experience",
+  "header.nav.items.experience.title": "Experience",
+  //---
+  "header.nav.items.projects.id": "#projects",
+  "header.nav.items.projects.label": "projects",
+  "header.nav.items.projects.title": "Projects",
+  //---
+  "header.nav.items.about.me.id": "#about-me",
+  "header.nav.items.about.me.label": "about-me",
+  "header.nav.items.about.me.title": "About Me",
+  //---
+  "header.nav.items.contact.id": "#contact",
+  "header.nav.items.contact.label": "contact",
+  "header.nav.items.contact.title": "Contact",
+  // Hero Section
+  "hero.image.profile.alt":
+    "A picture of me, Agustin Torres, wearing a blue shirt.",
+  "hero.badge.webpages": "web pages",
+  "hero.badge.apps": "applications",
+  "hero.badge.database": "databases",
+  "hero.badge.design": "design",
+  "hero.greeting": "Hey! I´m Agus",
+  "hero.years.of.experience": "+1 year of experience.",
+  "hero.description":
+    "Studying software development and collaborating on third-party projects. I have extensive knowledge for building your next web project or application.",
+  "hero.download.cv": "Resume",
+  "hero.download.cv.file.name": "torres-agustin-resume",
+  "hero.download.cv.file.href": "/torres-facundo-cv-en.pdf",
+  // Experience Section
+  "experience.section.title": "Experience",
+  // Red Mascotera
+  "experience.red.mascotera.title": "Red Mascotera - Charitable Project",
+  "experience.red.mascotera.date": "november 2023 - may 2024",
+  "experience.red.mascotera.description":
+    "Leading the construction of the webpage alongside a team of frontend developers. Conducting meetings with teams from other technical areas to assess project status, identify areas for improvement, and monitor progress. Utilizing project management tools to facilitate transparent collaboration across development teams.",
+  // ITBA
+  "experience.itba.title":
+    "Escuela de Innovación ITBA - Training Programme in FullStack Developer",
+  "experience.itba.date": "august 2023 - november 2023",
+  "experience.itba.description":
+    "Designing, laying out, and developing a fictitious home banking platform with a team of three colleagues. Overseeing the entire app construction process, from creating the web interface to implementing a REST API for data creation, modification, and updates. Responsible for designing, developing API endpoints, and establishing frontend-backend connectivity.",
+  // Don Bosco Labs
+  "experience.don.bosco.labs.title":
+    "Instituto Web - IT Experience in Software Development",
+  "experience.don.bosco.labs.date": "august 2023 - october 2023",
+  "experience.don.bosco.labs.description":
+    "Laying out and developing a platform for a clinical analysis laboratory system. Responsible for constructing the homepage. Participated in weekly meetings to evaluate new features and gain insight into other areas of development.",
+  // Femyp SRL
+  "experience.femyp.title": "Femyp SRL - Internship in Elevator Company",
+  "experience.femyp.date": "october 2022 - november 2022",
+  "experience.femyp.description":
+    "In charge of designing elevator components and furniture for the proposal of the company's new technical office.",
+  // Project Section
+  "project.section.title": "Projects",
+  "project.section.webpage.btn.label": "Web Page",
+  // Homebanking
+  "project.homebanking.description":
+    "Project for a FullStack Developer training program, alongside a development team. User-friendly and easily navigable home banking web application. REST API and authentication developed using Django REST Framework.",
+  "project.homebanking.img.alt":
+    "Preview of the web-developed home banking application. The platform is called ITBANK, where banking operations can be carried out conveniently from a mobile device.",
+  // Don Bosco Labs
+  "project.don.bosco.description":
+    "Clinical analysis laboratory system developed for a practical project. Implemented within a Scrum framework. Responsible for developing the laboratory's website.",
+  "project.don.bosco.img.alt":
+    "Preview of the clinical analysis laboratory system, where the initial section features an image of a woman wearing laboratory attire with arms crossed.",
+  // Entrevista Tecnica Liftun
+  "project.tech.interview.title": "Technical Interview - Liftun",
+  "project.tech.interview.description":
+    "Interface that enables users to configure various fundamental elements to create a sustainable action for the environment. REST API and database for storing the actions.",
+  "project.tech.interview.img.alt":
+    "Preview of a form for creating cards that indicate a sustainable action for the planet.",
+  // E-Commerce
+  "project.ecommerce.description":
+    "Project based on a figma design from another user. The design was changed in order to develope certain features like a functional searching navigation bar, a language toggler button and a carrousel of images. It uses the Fake Store API to fetch products.",
+  "project.ecommerce.img.alt":
+    "Preview of the ecommerce web home which shows a carrousel of products to purchase",
+  // About Me Section
+  "about.me.section.title": "About Me",
+  "about.me.section.img.profile.alt": "A picture of me, Torres Agustin",
+  /* 
+            My name is Agustin and I am 20 years old. I graduated as an [electromechanical technician] in high school, and I am currently studying [Computer Engineering] at the University of Buenos Aires (UBA).
+
+            I have been studying programming since 2022, starting with [web development.] During my last year of high school, I had the opportunity to complement my knowledge in a project involving [electronics, automation, and programming.] I have also collaborated in the construction of third-party software projects.
+
+            I consider myself a [curious, responsible, and focused individual.] I firmly believe that the details in a project make a difference; in this way, we highlight the personal touch of our product and/or service.
+        */
+  // First Paragraph
+  "about.me.fp.s1":
+    "My name is Agustin and I am 21 years old. I graduated as an",
+  "about.me.fp.kw1": "electromechanical technician",
+  "about.me.fp.s2": "in high school, and I am currently studying",
+  "about.me.fp.kw2": "Computer Engineering",
+  "about.me.fp.s3": "at the University of Buenos Aires (UBA).",
+  // Second Paragraph
+  "about.me.sp.s1":
+    "I have been studying programming since 2022, starting with",
+  "about.me.sp.kw1": "web development.",
+  "about.me.sp.s2":
+    "During my last year of high school, I had the opportunity to complement my knowledge in a project involving",
+  "about.me.sp.kw2": "electronics, automation, and programming.",
+  "about.me.sp.s3":
+    "I have also collaborated in the construction of third-party software projects.",
+  // Third Paragraph
+  "about.me.tp.s1": "I consider myself a",
+  "about.me.tp.kw1": "curious, responsible, and focused individual.",
+  "about.me.tp.s2":
+    "I firmly believe that the details in a project make a difference; in this way, we highlight the personal touch of our product and/or service.",
+  // Footer Section
+  "footer.portfolio.develop.with.title": "Developed with Astro",
+};
+
+export default en;
---
+++ src/i18n/lang/spanish.ts
@@ -0,0 +1,130 @@
+const es = {
+  // Head
+  "head.title": "Torres Agustin - Portafolio",
+  "head.description": "Mi portafolio personal de desarrollador web",
+  // Body
+  // Message Container
+  "message.container": "Email Copiado",
+  // Header Section
+  "header.top": "Inicio",
+  "header.experience": "Experiencia",
+  "header.projects": "Proyectos",
+  "header.about.me": "Sobre Mí",
+  "header.contact": "Contacto",
+  // Nav Items
+  "header.nav.items.top.id": "#top",
+  "header.nav.items.top.label": "top",
+  "header.nav.items.top.title": "Inicio",
+  //---
+  "header.nav.items.experience.id": "#experience",
+  "header.nav.items.experience.label": "experience",
+  "header.nav.items.experience.title": "Experiencia",
+  //---
+  "header.nav.items.projects.id": "#projects",
+  "header.nav.items.projects.label": "projects",
+  "header.nav.items.projects.title": "Proyectos",
+  //---
+  "header.nav.items.about.me.id": "#about-me",
+  "header.nav.items.about.me.label": "about-me",
+  "header.nav.items.about.me.title": "Sobre Mí",
+  //---
+  "header.nav.items.contact.id": "#contact",
+  "header.nav.items.contact.label": "contact",
+  "header.nav.items.contact.title": "Contacto",
+  // Hero Section
+  "hero.image.profile.alt":
+    "Una foto mia, Agustin Torres, vestido con una camisa azul.",
+  "hero.badge.webpages": "páginas web",
+  "hero.badge.apps": "aplicaciones",
+  "hero.badge.database": "bases de datos",
+  "hero.badge.design": "diseño",
+  "hero.greeting": "Un gusto! Soy Agus",
+  "hero.years.of.experience": "+1 año de experiencia.",
+  "hero.description":
+    "Estudiando desarrollo de software y colaborando en proyectos de terceros. Cuento con amplios conocimientos para la construcción de tu próximo proyecto web o aplicación.",
+  "hero.download.cv": "Descargar CV",
+  "hero.download.cv.file.name": "torres-agustin-cv",
+  "hero.download.cv.file.href": "/torres-facundo-cv-es.pdf",
+  // Experience Section
+  "experience.section.title": "Experiencia",
+  // Red Mascotera
+  "experience.red.mascotera.title": "Red Mascotera - Proyecto Solidario",
+  "experience.red.mascotera.date": "noviembre 2023 - mayo 2024",
+  "experience.red.mascotera.description":
+    "A cargo de la construcción de la página web junto a un equipo de desarrolladores frontend. Reuniones con equipos de otras áreas técnicas para evaluar el estado, la mejora, y el avance del proyecto. Uso de herramientas de gestion de proyectos para la transparencia del trabajo con otras áreas de desarrollo.",
+  // ITBA
+  "experience.itba.title":
+    "Escuela de Innovación ITBA - Programa de Capacitación Fullstack Developer",
+  "experience.itba.date": "agosto 2023 - noviembre 2023",
+  "experience.itba.description":
+    "Diseño, maquetación, y desarrollo de un homebanking ficticio junto a un equipo de 3 compañeros. Proceso completo de construcción de la app desde la creación de la vista web hasta la implementación de una REST-API para la creación, modificación, y actualización de datos. A cargo del diseño, desarrollo de los endpoints de la API, y la conexión frontend-backend.",
+  // Don Bosco Labs
+  "experience.don.bosco.labs.title":
+    "Instituto Web - Experiencia IT en Desarrollo de Software",
+  "experience.don.bosco.labs.date": "agosto 2023 - octubre 2023",
+  "experience.don.bosco.labs.description":
+    "Maquetación y desarrollo de una plataforma para un sistema de laboratorio de análisis clínicos. A cargo de la construcción de la página de inicio o home-page. Participé en reuniones semanales para la evaluación de nuevas caracteristicas y el aprendizaje de otras áreas de desarrollo.",
+  // Femyp SRL
+  "experience.femyp.title": "Femyp SRL - Pasantia en Empresa de Ascensores",
+  "experience.femyp.date": "octubre 2022 - noviembre 2022",
+  "experience.femyp.description":
+    "A cargo del diseño de piezas de ascensores, y de muebleria para la propuesta de la nueva oficina técnica de la empresa.",
+  // Project Section
+  "project.section.title": "Proyectos",
+  "project.section.webpage.btn.label": "Página Web",
+  // Homebanking
+  "project.homebanking.description":
+    "Proyecto para un programa de capacitación en FullStack Developer, junto a un equipo de desarrollo. Aplicación web de homebanking, amigable al usuario y de facil navegación. API REST y autenticación desarrollada con django-rest-framework.",
+  "project.homebanking.img.alt":
+    "Prevista de la aplicación de homebanking desarrollada para la web. La plataforma se llama ITBANK y allí se pueden realizar operaciones bancarias desde a comodidad de un celular.",
+  // Don Bosco Labs
+  "project.don.bosco.description":
+    "Sistema de laboratorio de análisis clínicos desarrollada para una practica. Marco de trabajo Scrum. Encargado de desarrollar la página web del laboratorio.",
+  "project.don.bosco.img.alt":
+    "Prevista del sistema de laboratorio de análisis clínicos en donde lo primero que se muestra es la sección de inicio con una imagen de una mujer con vestimenta de laboratorio en brazos cruzados.",
+  // Entrevista Tecnica Liftun
+  "project.tech.interview.title": "Entrevista Técnica - Liftun",
+  "project.tech.interview.description":
+    "Interfaz que permite a los usuarios configurar distintos elementos fundamentales para crear una acción sostenible para el medio ambiente. API REST y base de datos para almacenar las acciones.",
+  "project.tech.interview.img.alt":
+    "Prevista de un formulario para la creación de tarjetas que indican una acción sostenible para el planeta.",
+  // E-Commerce
+  "project.ecommerce.description":
+    "Proyecto basado en un diseño de figma creado por otro usuario. El diseño fue cambiado y se añadieron caracteristicas como una barra de busqueda funcional, un botón para el cambio de idioma y un carrousel de imágenes. Usa la API de Fake Store Api.",
+  "project.ecommerce.img.alt":
+    "Prevista del home de la página web de ecommerce, el cual muestra un carrousel de imagenes de productos para comprar",
+  // About Me Section
+  "about.me.section.title": "Sobre Mí",
+  "about.me.section.img.profile.alt": "Una foto de mí, Torres Agustin",
+  /*
+            Mi nombre es Agustin y tengo 20 años. Me gradué como [técnico electromecánico] en la secundaria y actualmente estoy cursando mis estudios universitarios para [Ingenieria Informática] en la Universidad de Buenos Aires (UBA).
+            
+            Estudio programación desde 2022, empezando por el [desarrollo de páginas web.] Durante mi último año de secundaria tuve la oportunidad de complementar mis conocimientos en un proyecto de [electrónica, automatización, y programación.] Tambien he colaborado en la construcción de proyectos de software de terceros.
+            
+            Me considero una persona [curiosa, responsable y centrada.] Considero firmemente que los detalles en un proyecto marcan la diferencia, de esa forma destacamos el sello personal de nuestro producto y/o servicio.
+        */
+  // First Paragraph
+  "about.me.fp.s1": "Mi nombre es Agustin y tengo 21 años. Me gradué como",
+  "about.me.fp.kw1": "técnico electromecánico",
+  "about.me.fp.s2":
+    "en la secundaria y actualmente estoy cursando mis estudios universitarios para",
+  "about.me.fp.kw2": "Ingenieria Informática",
+  "about.me.fp.s3": "en la Universidad de Buenos Aires (UBA).",
+  // Second Paragraph
+  "about.me.sp.s1": "Estudio programación desde 2022, empezando por el",
+  "about.me.sp.kw1": "desarrollo de páginas web.",
+  "about.me.sp.s2":
+    "Durante mi último año de secundaria tuve la oportunidad de complementar mis conocimientos en un proyecto de",
+  "about.me.sp.kw2": "electrónica, automatización, y programación.",
+  "about.me.sp.s3":
+    "Tambien he colaborado en la construcción de proyectos de software de terceros.",
+  // Third Paragraph
+  "about.me.tp.s1": "Me considero una persona",
+  "about.me.tp.kw1": "curiosa, responsable y centrada.",
+  "about.me.tp.s2":
+    "Considero firmemente que los detalles en un proyecto marcan la diferencia, de esa forma destacamos el sello personal de nuestro producto y/o servicio.",
+  // Footer Section
+  "footer.portfolio.develop.with.title": "Desarrollado con Astro",
+};
+
+export default es;
